01838bd5df806b11cf25088bfdab9c3e6f5a55ad
Husni <husniwfayo@gmail.com>
2025-08-08 20:46:10 +0530

    added dictionary for proper data structure

--- app/model/keystroke_analyzer.py
+++ app/model/keystroke_analyzer.py
@@ -6,12 +6,14 @@ class KeystrokeAnalyzer:
     def __init__(self):
         self.keystrokes = []
         self.mistakes = []
-        self.typing_speed = 0
-        self.error_rate = 0
-        self.pause_rate = 0
         self.start_time = 0
         self.end_time = 0
-
+        self.listener = None
+        self.keystroke_data = {
+            "typing_speed": 0,
+            "error_rate": 0,
+            "pause_rate": 0,
+        }
 
     def start_tracker(self):
         self.start_time = time.time()
@@ -42,36 +44,36 @@ class KeystrokeAnalyzer:
 
     def calculate_typing_speed(self):
         if len(self.keystrokes) < 2:
-            self.typing_speed = 0
-            return self.typing_speed
+            self.keystroke_data["typing_speed"] = 0
+            return self.keystroke_data["typing_speed"]
 
         duration = self.end_time - self.start_time
         if duration == 0:
-            self.typing_speed = 0
+            self.keystroke_data["typing_speed"] = 0
         else:
-            self.typing_speed = round(len(self.keystrokes) / duration, 2)
-        return self.typing_speed
+            self.keystroke_data["typing_speed"] = round(len(self.keystrokes) / duration, 2)
+        return self.keystroke_data["typing_speed"]
 
     def calculate_error_rate(self):
         if len(self.keystrokes) == 0:
-            self.error_rate = 0
-            return self.error_rate
+            self.keystroke_data["error_rate"] = 0
+            return self.keystroke_data["error_rate"]
 
         total_keys = len(self.keystrokes)
         total_errors = len(self.mistakes)
 
-        self.error_rate = round((total_errors / total_keys) * 100, 2)
-        return self.error_rate
+        self.keystroke_data["error_rate"] = round((total_errors / total_keys) * 100, 2)
+        return self.keystroke_data["error_rate"]
 
     def calculate_pause_rate(self):
         if len(self.keystrokes) < 2:
-            self.pause_rate = 0
-            return self.pause_rate
+            self.keystroke_data["pause_rate"] = 0
+            return self.keystroke_data["pause_rate"]
 
         total_gap = 0
         for i in range(1, len(self.keystrokes)):
             total_gap += self.keystrokes[i][1] - self.keystrokes[i - 1][1]
 
-        self.pause_rate = round(total_gap / (len(self.keystrokes) - 1), 2)
-        return self.pause_rate
+        self.keystroke_data["pause_rate"] = round(total_gap / (len(self.keystrokes) - 1), 2)
+        return self.keystroke_data["pause_rate"]
 
